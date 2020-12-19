feb3b11e37deeba05a93d0c73c5b599b3be9d25e
mikigv <mikiasgv@gmail.com>
2020-12-19 15:37:42 +0300

    Error handling module

--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 const express = require('express');
-const dotenv =require('dotenv');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 //load environment variables
 dotenv.config({ path: './config/config.env' });
 
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+//body parser
+app.use(express.json());
+
+//Dev login middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 //Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server run in ${process.env.NODE_ENV} and listne on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server run in ${process.env.NODE_ENV} and listne on port ${PORT}`));
+
+process.on('uncaughtExceptionMonitor', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+
+    //close server and exit
+    server.close(() => process.exit(1));
+});
